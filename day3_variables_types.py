d842f6c8db53658a00dc3db86d263a6abfe41cc8
InsightLedger <insightledger059@gmail.com>
2026-03-19 23:31:09 +0530

    Create day3_variables_types.py
    # Day 3 - Variables and Data Types
    # This program shows basic data types in Python
    
    # Integer (int) example
    age = 21
    year = 2026
    print(age)
    
    # Float example
    price = 99.99
    print(price)
    
    # String (str) example
    name = "Emma"
    city = "Delhi"
    print(name, city)
    
    # Boolean (bool) example
    is_student = True
    print(is_student)

---
+++ day3_variables_types.py
@@ -0,0 +1,48 @@
+Types of variables 
+1)integer(int)-number
+2)float-decimals
+3)str-text
+4)bool-true/false
+
+1. Integer (int)
+example 
+age = 21
+year = 2026
+print(age)
+
+3. String (str)
+example
+Text or characters.
+name = "Emma"
+city = "Delhi"
+print(name)
+
+4. Boolean (bool)
+Only True or False.
+example
+is_student = True
+is_logged_in = False
+print(is_student)
+
+#Few more I don't now what to name it though. 🙂
+
+Multiple Variables
+example 
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+Same Value to Multiple Variables
+example 
+a = b = c = 10
+print(a, b, c)
+
+
+
+practices example 
+name = "John"
+age = 22
+height = 5.8
+
+print(name)
+print(age)
+print(height)
